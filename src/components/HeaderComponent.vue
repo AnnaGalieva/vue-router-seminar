10d2486712909bba484161bb5bd732ce64cfe207
Anna <anutasash@mail.ru>
2023-08-25 22:31:02 +0300

    seminar

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <router-link v-for="link in links" :key="link.id" :to="link.url">
+            {{ link.title }}</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+
+    data() {
+        return {
+            links: [
+                { id: 1, title: 'home', url: '/' },
+                { id: 2, title: 'about', url: '/about' },
+                { id: 3, title: 'contact', url: '/contact' },
+                { id: 4, title: 'catalog', url: '/catalog' },
+            ]
+        };
+    },
+
+};
+// https://v3.router.vuejs.org/ru/guide/advanced/meta.html
+// https://tproger.ru/articles/routing-in-vue/
+// <router-link :to="{ name: 'user', params: { userId: 123 }}">Пользователь</router-link>
+// https://v3.router.vuejs.org/ru/api/#router-resolve
+</script>
+
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+import Contact from '@/pages/Contact.vue'
+import NotFound from '@/pages/NotFound.vue'
+import Catalog from '@/pages/Catalog.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/About',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/Contact',
+        name: 'Contact',
+        component: Contact
+    },
+    {
+        path: '/Catalog/:pageNumber?',
+        name: 'Catalog',
+        component: Catalog
+    },
+    {
+        path: '/:catchAll(.*)',
+        name: 'NotFound',
+        component: NotFound
+    },
+
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
